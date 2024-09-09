6c5d5be464d7182a33598881119853d7fab7379e
zhengHanTsai <11046064@ntub.edu.tw>
2024-09-10 00:16:30 +0800

    新增 - 對話方塊調整

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,23 @@
 import Layout from '@/components/layout/Layout';
 import styles from '@styles/pages/index.module.scss';
 import Image from 'next/image';
+import { useState } from 'react';
 
 export default function Home() {
   const userName = 'Justin';
+  const [isTalk, setIsTalk] = useState(false);
+
+  const StartVoiceInput = async () => {
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia({ 'audio': true });
+
+      stream.getTracks().forEach(track => track.stop());
+      alert(stream.getTracks().forEach(track => track.stop()));
+      setIsTalk(true);
+    } catch (err) {
+      alert(err);
+    }
+  };
 
   return (
     <Layout>
@@ -17,14 +31,15 @@ export default function Home() {
             <span className={styles.userName}>{userName}您好，歡迎使用智能聊天小夥伴 !</span>
           </div>
 
-          <button className={styles.voiceButton}>
-            <Image src="/images/microphone.png" alt="Microphone" width={100} height={60} className={styles.voiceButtonImage}/>
-          </button>
-
-          <div className={styles.actions}>
-            <p className={styles.voiceInput}>請開始說話</p>
-            <button className={styles.stopButton}>結束錄音</button>
-          </div>
+          { isTalk ?
+            <div className={styles.actions}>
+              <p className={styles.voiceInput} onClick={StartVoiceInput}>請開始說話</p>
+              <button className={styles.stopButton}>結束錄音</button>
+            </div> :
+            <button className={styles.voiceButton} onClick={() => setIsTalk(true)}>
+              <Image src="/images/microphone.png" alt="Microphone" width={100} height={60} className={styles.voiceButtonImage}/>
+            </button>
+          }
 
         </article>
       </div>
